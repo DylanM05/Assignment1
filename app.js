e31b4d8c9139b7b93ec9733de16500f4abd07fbc
Dylan McMullen <dylan.mcmullen@live.com>
2023-10-07 18:45:41 -0400

    Added submit function for contact page

--- app.js
+++ app.js
@@ -45,4 +45,5 @@ app.use(function(err, req, res, next) {
 
 
 
+
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,23 @@ router.get('/contact', (req, res) => {
   res.render('contact');
 });
 
+// POST request to handle form submission
+router.post('/contact', function (req, res, next) {
+  // Capture user information from the form
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message;
+
+  // Perform any necessary processing with the captured data
+
+  // Redirect back to the Home Page (you can change the URL as needed)
+  res.redirect('/');
+
+  // You can also send a success message if needed
+ res.render('success', { message: 'Message sent successfully!' });
+});
+
+
 // Define similar routes for other pages
 
 module.exports = router;
